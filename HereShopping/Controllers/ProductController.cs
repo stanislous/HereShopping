0b6a07fad497e9cfe0251d03bc702bc77140ffc0
vanderputt <vanderputt@thesoftwarepractice.com>
2019-03-06 14:45:15 +0530

    Create a product and see all products

--- HereShopping/Controllers/ProductController.cs
+++ HereShopping/Controllers/ProductController.cs
@@ -1,21 +1,50 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ShoppingBOL;
+using ShoppingBLL;
 
 namespace HereShopping.Controllers
 {
     public class ProductController : Controller
     {
+        private ProductBL productBl;
+        public ProductController()
+        {
+            productBl = new ProductBL();
+        }
+
         public IActionResult Index( )
         {
             return View();
         }
 
+        #region Create Product
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            productBl.CreateProduct(product);
+            productBl.SaveChanges();
+            return RedirectToAction("Create", "Product");
+        }
+        #endregion
+
+        #region See All Products
+
+        public IActionResult SeeAll( )
+        {
+            IEnumerable<Product> AllProducts = productBl.GetAllProducts();
+            return View(AllProducts);
+        }
+        #endregion
     }
 }
